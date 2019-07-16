f491bf4aeb229ee65edb5df6a07ca6d08557144f
SnehithGaddam <52900396+SnehithGaddam@users.noreply.github.com>
2019-07-16 16:07:27 -0500

    RestAPI

---
+++ rest-api-helpers-master/rest-api-helpers-master/src/RestApiHelpers/Validation/ValidateActionParametersAttribute.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace RestApiHelpers.Validation
+{
+    /// <summary>
+    /// Validates all the validation attributes placed directly on action method parameters.
+    /// </summary>
+    /// <remarks>
+    /// The framework by default doesn't evaluate the validation attributes put directly on action method parameters. It only evaluates the attributes put on the properties of the model types.
+    /// This filter validates the attributes placed directly on action method parameters, and adds all the validation errors to the ModelState collection.
+    /// </remarks>
+    public class ValidateActionParametersAttribute : ActionFilterAttribute
+    {
+        public ValidateActionParametersAttribute()
+        {
+            Order = 1;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
+
+            if (descriptor != null)
+            {
+                var parameters = descriptor.MethodInfo.GetParameters();
+
+                foreach (var parameter in parameters)
+                {
+                    var argument = context.ActionArguments.ContainsKey(parameter.Name) ?
+                        context.ActionArguments[parameter.Name] : null;
+
+                    EvaluateValidationAttributes(parameter, argument, context.ModelState);
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        private void EvaluateValidationAttributes(ParameterInfo parameter, object argument, ModelStateDictionary modelState)
+        {
+            var validationAttributes = parameter.CustomAttributes;
+
+            foreach (var attributeData in validationAttributes)
+            {
+                var attributeInstance = CustomAttributeExtensions.GetCustomAttribute(parameter, attributeData.AttributeType);
+
+                var validationAttribute = attributeInstance as ValidationAttribute;
+
+                if (validationAttribute != null)
+                {
+                    var isValid = validationAttribute.IsValid(argument);
+                    if (!isValid)
+                    {
+                        modelState.AddModelError(parameter.Name, validationAttribute.FormatErrorMessage(parameter.Name));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ rest-api-helpers-master/rest-api-helpers-master/test/RestApiHelpers.Test/Validation/ReturnBadRequestOnModelErrorTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Moq;
+using RestApiHelpers.Validation;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RestApiHelpers.Test.Validation
+{
+    public class ReturnBadRequestOnModelErrorTest
+    {
+        [Fact]
+        public void OnActionExecuting_ModelValid_NoResultSet()
+        {
+            var sut = new ReturnBadRequestOnModelError();
+
+            var modelStateDictionary = new ModelStateDictionary();
+            var httpContext = new Mock<HttpContext>();
+            var actionContext = new ActionContext(httpContext.Object, new Microsoft.AspNetCore.Routing.RouteData(), new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor(), modelStateDictionary);
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                new object());
+
+            // Act
+            sut.OnActionExecuting(actionExecutingContext);
+
+            Assert.Null(actionExecutingContext.Result);
+        }
+
+        [Fact]
+        public void OnActionExecuting_ModelInvalid_BadRequestResultSet()
+        {
+            var sut = new ReturnBadRequestOnModelError();
+
+            var modelStateDictionary = new ModelStateDictionary();
+            var httpContext = new Mock<HttpContext>();
+            var actionContext = new ActionContext(httpContext.Object, new Microsoft.AspNetCore.Routing.RouteData(), new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor(), modelStateDictionary);
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                new object());
+            modelStateDictionary.AddModelError("error", "error");
+
+            // Act
+            sut.OnActionExecuting(actionExecutingContext);
+
+            Assert.IsType<BadRequestObjectResult>(actionExecutingContext.Result);
+        }
+    }
+}
